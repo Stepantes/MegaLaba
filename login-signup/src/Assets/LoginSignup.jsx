0c3b16613d560cc961f16a00fd353063954c21cc
Stepantes <ugra2004@gmail.com>
2025-05-05 00:05:37 +0500

    Страница входа и регистрации; личный кабинет

---
+++ login-signup/src/Assets/LoginSignup.jsx
@@ -0,0 +1,28 @@
+import React from 'react'
+
+const LoginSignup = () => {
+  return (
+    <div className='container'>
+        <div className='header'>
+            <div className='text'>Sign Up</div>
+            <div className='underline'></div>
+        </div>
+     <div className='inputs'>
+        <div className='input'>
+            <img src="" alt="" />
+            <input type="text" />
+        </div>
+        <div className='input'>
+            <img src="" alt="" />
+            <input type="password" />
+        </div>
+      </div>
+      <div className="submit-container">
+        <div className="Submit">Sign Up</div>
+        <div className="Submit">Login</div>
+      </div>
+    </div>
+  )
+}
+
+export default LoginSignup
